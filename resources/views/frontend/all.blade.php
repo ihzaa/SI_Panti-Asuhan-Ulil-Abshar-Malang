962d5f94527e6ce8e62365224039a5f41ac03bc9
antoko <Totonantoko@gmail.com>
2020-08-05 06:13:31 +0700

    Add Csrf

--- resources/views/frontend/all.blade.php
+++ resources/views/frontend/all.blade.php
@@ -3,6 +3,7 @@
   <head>
     <title>@yield('JudulHalaman')</title>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
