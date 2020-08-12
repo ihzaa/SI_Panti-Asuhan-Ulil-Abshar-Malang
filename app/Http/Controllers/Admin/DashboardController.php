d90947ab65688434cd0baf871cf0b4a0fc7fd071
Ihza Ahmad <ihzaahmad0@gmail.com>
2020-08-13 02:06:08 +0800

    fix chart donasi

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,6 +10,7 @@ use App\Models\Frontend\manager;
 use App\Models\Frontend\Produk;
 use App\Models\Frontend\ProfilAnak;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -23,6 +24,14 @@ class DashboardController extends Controller
         $data['donasi_masuk'] = DonasiMasuk::count();
         $masuk = DonasiMasuk::pluck('donasi_id');
         $data['donasi_belum'] = Donasi::whereNotIn('id', $masuk)->count();
+        $data['donasi'] = array();
+        $data['total_donasi'] = 0;
+        for ($i = 1; $i <= 12; $i++) {
+            $total = DB::table('donasis')->select(DB::raw('sum(total_donasi) as total'))->whereIn('id', $masuk)->whereMonth('created_at', '=', "$i")->whereYear('created_at', '=', date('Y'))->pluck('total');
+            array_push($data['donasi'], $total[0] == null ? "0" : $total[0]);
+            $data['total_donasi'] += $total[0];
+        }
+
         return view('admin.pages.dashboard', compact('data'));
     }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -93,56 +93,56 @@
                 <div class="inner">
                     <h3>{{$data['pengurus']}}</h3>
 
-                    <p>Donasi Masuk</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-user"></i>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-6">
-            <!-- small card -->
-            <div class="small-box bg-info">
-                <div class="inner">
-                    <h3>{{$data['anak']}}</h3>
-
-                    <p>Donasi</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-child"></i>
-                </div>
-            </div>
-        </div> --}}
+        <p>Donasi Masuk</p>
     </div>
-    <div class="row">
-        <div class="col-lg-8">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title">Donasi tahun {{date("Y")}}</h3>
-                        {{-- <a href="javascript:void(0);">View Report</a> --}}
-                    </div>
+    <div class="icon">
+        <i class="fas fa-user"></i>
+    </div>
+</div>
+</div>
+<div class="col-lg-3 col-6">
+    <!-- small card -->
+    <div class="small-box bg-info">
+        <div class="inner">
+            <h3>{{$data['anak']}}</h3>
+
+            <p>Donasi</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-child"></i>
+        </div>
+    </div>
+</div> --}}
+</div>
+<div class="row">
+    <div class="col-lg-8">
+        <div class="card">
+            <div class="card-header border-0">
+                <div class="d-flex justify-content-between">
+                    <h3 class="card-title">Donasi tahun {{date("Y")}}</h3>
+                    {{-- <a href="javascript:void(0);">View Report</a> --}}
                 </div>
-                <div class="card-body">
-                    <div class="d-flex">
-                        <p class="d-flex flex-column">
-                            <span class="text-bold text-lg">Rp. 820</span>
-                            <span>Total Donasi Diterima</span>
-                        </p>
-                        {{-- <p class="ml-auto d-flex flex-column text-right">
+            </div>
+            <div class="card-body">
+                <div class="d-flex">
+                    <p class="d-flex flex-column">
+                        <span class="text-bold text-lg">Rp. {{number_format($data['total_donasi'], 0, '.', '.')}}</span>
+                        <span>Total Donasi Diterima</span>
+                    </p>
+                    {{-- <p class="ml-auto d-flex flex-column text-right">
                             <span class="text-success">
                                 <i class="fas fa-arrow-up"></i> 12.5%
                             </span>
                             <span class="text-muted">Since last week</span>
                         </p> --}}
-                    </div>
-                    <!-- /.d-flex -->
+                </div>
+                <!-- /.d-flex -->
 
-                    <div class="position-relative mb-4">
-                        <canvas id="chart_donasi" height="150"></canvas>
-                    </div>
+                <div class="position-relative mb-4">
+                    <canvas id="chart_donasi" height="150"></canvas>
+                </div>
 
-                    {{-- <div class="d-flex flex-row justify-content-end">
+                {{-- <div class="d-flex flex-row justify-content-end">
                         <span class="mr-2">
                             <i class="fas fa-square text-primary"></i> This Week
                         </span>
@@ -151,54 +151,54 @@
                             <i class="fas fa-square text-gray"></i> Last Week
                         </span>
                     </div> --}}
-                </div>
             </div>
-            <!-- /.card -->
         </div>
-        <div class="col-md-4">
-            <!-- Info Boxes Style 2 -->
-            <div class="info-box mb-3 bg-primary">
-                <span class="info-box-icon"><i class="fas fa-hand-holding-heart"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Donasi Masuk</span>
-                <span class="info-box-number">{{$data['donasi_masuk']}}</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-            <div class="info-box mb-3 bg-success">
-                <span class="info-box-icon"><i class="fas fa-check-circle"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Donasi yang Belum Dikonfirmasi</span>
-                <span class="info-box-number">{{$data['donasi_belum']}}</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-            <div class="info-box mb-3 bg-danger">
-                <span class="info-box-icon"><i class="fas fa-handshake"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Donatur Tetap</span>
-                    <span class="info-box-number">114,381</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-            <div class="info-box mb-3 bg-info">
-                <span class="info-box-icon"><i class="fas fa-house-user"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Kamar</span>
-                    <span class="info-box-number">163,921</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
+        <!-- /.card -->
     </div>
+    <div class="col-md-4">
+        <!-- Info Boxes Style 2 -->
+        <div class="info-box mb-3 bg-primary">
+            <span class="info-box-icon"><i class="fas fa-hand-holding-heart"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Donasi Masuk</span>
+                <span class="info-box-number">{{$data['donasi_masuk']}}</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+        <div class="info-box mb-3 bg-success">
+            <span class="info-box-icon"><i class="fas fa-check-circle"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Donasi yang Belum Dikonfirmasi</span>
+                <span class="info-box-number">{{$data['donasi_belum']}}</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+        <div class="info-box mb-3 bg-danger">
+            <span class="info-box-icon"><i class="fas fa-handshake"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Donatur Tetap</span>
+                <span class="info-box-number">114,381</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+        <div class="info-box mb-3 bg-info">
+            <span class="info-box-icon"><i class="fas fa-house-user"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Kamar</span>
+                <span class="info-box-number">163,921</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+</div>
 </div>
 
 @endsection
@@ -213,7 +213,7 @@
         labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli','Agustus','September','Oktober','November','Desember'],
         datasets: [{
             label: 'Total donasi',
-            data: [12, 19, 3, 5, 2, 3],
+            data: {!! json_encode($data['donasi']) !!},
             borderWidth: 1,
             fill: false,
             borderColor: 'rgba(15, 255, 135, 1)'
