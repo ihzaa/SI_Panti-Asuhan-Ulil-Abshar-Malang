3b00cb8ecebd739ab0ac1f3b228777320d822a1b
Ihza Ahmad <ihzaahmad0@gmail.com>
2020-08-27 22:29:28 +0800

    frontend add recapitulation charity

--- resources/views/frontend/pages/Home/Components/donasi_now.blade.php
+++ resources/views/frontend/pages/Home/Components/donasi_now.blade.php
@@ -1,77 +1,183 @@
 <section class="ftco-section bg-light">
-  <div class="container">
-    <div class="row justify-content-center pb-3">
-      <div class="col-md-7 heading-section text-center ftco-animate">
-        {{-- <span class="subheading">Foundation Grants Projects</span> --}}
-        <h2 class="mb-4">Donasi Bulan Ini</h2>
-        {{-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p> --}}
-      </div>
-    </div>
-    <div class="row justify-content-center ftco-animate">
-      <div class="col-md-8">
-        <div class="featured-causes">
-          <div class="progress" style="height:50px; flex-direction: column; justify-content: center;">
-              <p style="position: absolute;
+    <div class="container">
+        <div class="row justify-content-center pb-3">
+            <div class="col-md-7 heading-section text-center ftco-animate">
+                {{-- <span class="subheading">Foundation Grants Projects</span> --}}
+                <h2 class="mb-4">Donasi Bulan Ini</h2>
+                {{-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p> --}}
+            </div>
+        </div>
+        <div class="row justify-content-center ftco-animate">
+            <div class="col-md-8">
+                <div class="featured-causes">
+                    <div class="progress" style="height:50px; flex-direction: column; justify-content: center;">
+                        <p style="position: absolute;
               margin-left: auto;
               margin-right: auto;
               left: 0;
               right: 0;
               text-align: center;
               "> {{$jml_hari-$day_now}} Hari Lagi</p>
-            @if ($donasi)
-              <div class="progress-bar progress-bar-striped" style="width:{{($day_now/$jml_hari)*100}}%; height:50px"></div>
-            @else
-              {{-- <div class="progress-bar progress-bar-striped" style="width:{{(0/$kebutuhan)*100}}%; height:50px"></div> --}}
-            @endif
-          </div>
-          <div class="text mt-4 d-md-flex">
-            <div class="one d-flex">
-              <div class="mr-4">
-                @if ($donasi)
-                  <h2>Rp <span class="number" data-number="{{$donasi->sum}}"></span> terkumpul</h2>
-                @else
-                  {{-- <h2>{{intval((0/$kebutuhan)*100)}}%</h2> --}}
-                @endif
-              </div>
+                        @if ($donasi)
+                        <div class="progress-bar progress-bar-striped"
+                            style="width:{{($day_now/$jml_hari)*100}}%; height:50px"></div>
+                        @else
+                        {{-- <div class="progress-bar progress-bar-striped" style="width:{{(0/$kebutuhan)*100}}%;
+                        height:50px">
+                    </div> --}}
+                    @endif
+                </div>
+                <div class="text mt-4 d-md-flex">
+                    <div class="one d-flex">
+                        <div class="mr-4">
+                            @if ($donasi)
+                            <h2>Rp <span class="number" data-number="{{$donasi->sum}}"></span> terkumpul</h2>
+                            @else
+                            {{-- <h2>{{intval((0/$kebutuhan)*100)}}%</h2> --}}
+                            @endif
+                        </div>
+                    </div>
+                    <div class="one text-md-right">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#donasiModal">
+                            Donasi Sekarang
+                        </button>
+                        {{-- <p><a href="#" class="btn btn-primary">Donate now</a></p> --}}
+                    </div>
+                </div>
             </div>
-            <div class="one text-md-right">
-              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#donasiModal">
-                Donasi Sekarang
-              </button>
-              {{-- <p><a href="#" class="btn btn-primary">Donate now</a></p> --}}
-            </div>
-          </div>
         </div>
-      </div>
-      @if (count($donatur_terbaru) >= 1)
+        @if (count($donatur_terbaru) >= 1)
         <div class="col-md-8" style="padding: 15px;
         margin: 15px;">
-          <h1 style="font-size: 20px;">Donasi Terbaru</h1>
-          <div class="row">
-            @if (count($donatur_terbaru) >= 1)
-              @foreach ($donatur_terbaru as $item)
-                <div class="col-md-4">
-                  <div style="background: #fff;
+            <h1 style="font-size: 20px;">Donasi Terbaru</h1>
+            <div class="row">
+                @if (count($donatur_terbaru) >= 1)
+                @foreach ($donatur_terbaru as $item)
+                <div class="col-md-4 mb-2">
+                    <div style="background: #fff;
                   text-align: center;
                   line-height: 7px;
                   padding: 17px;
                   border-radius: 10px;
                   background: #81e2c7;
                   color: #636363;">
-                    <p style="font-weight: bold;">{{$item->nama_donatur}}</p> 
-                    <p>Rp. {{number_format($item->total_donasi)}}</p>
-                    <p>{{date('d F Y', strtotime($item->created_at))}}</p>
-                  </div>
+                        <p style="font-weight: bold;">{{$item->nama_donatur}}</p>
+                        <p>Rp. {{number_format($item->total_donasi)}}</p>
+                        <p>{{date('d F Y', strtotime($item->created_at))}}</p>
+                    </div>
                 </div>
-              @endforeach
-            @else
-              <div class="col-md-12" style="text-align: center">
-                <p style="font-size: 1.1rem">Belum ada donasi saat ini.</p>
-              </div>
-            @endif
-          </div>
+                @endforeach
+                @else
+                <div class="col-md-12" style="text-align: center">
+                    <p style="font-size: 1.1rem">Belum ada donasi saat ini.</p>
+                </div>
+                @endif
+            </div>
         </div>
-      @endif
+        @endif
+        <div class="col-md-8" style="padding: 15px;margin: 15px;">
+            <div class="info-box mb-3" id="card_recap">
+                <div class="info-box-content">
+                    <span class="info-box-text mb-2"><strong>Rekapitulas Donasi</strong></span>
+                    <div class="row">
+                        <div class="col-md-6 mb-2">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="tahun">Tahun</label>
+                                </div>
+                                <select class="custom-select" id="tahun">
+                                    <option selected value="xx">Pilih tahun...</option>
+                                </select>
+                                <div class="input-group-append">
+                                    <a class="btn btn-success disabled" href="#" id="btn_unduh_tahun"
+                                        target="_blank">Unduh</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <fieldset disabled id="form_bulan">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <label class="input-group-text" for="bulan">Bulan</label>
+                                    </div>
+                                    <select class="custom-select" id="bulan">
+                                        <option selected value="xx">Pilih bulan...</option>
+                                    </select>
+                                    <div class="input-group-append">
+                                        <a class="btn btn-success disabled" href="#" id="btn_unduh_bulan"
+                                            target="_blank">Unduh</a>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>
+
+                    </div>
+
+
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
     </div>
-  </div>
+    </div>
 </section>
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+    fetch("{{route('all_user_get_tahun_donasi')}}")
+    .then(response => response.json())
+    .then(data => {
+        for(let i = 0; i < data.length ; i++){
+            $('#tahun').append(`
+            <option value="${data[i].year}">${data[i].year}</option>
+            `);
+        }
+    });
+    const bulan = ['','Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
+    $('#tahun').on('change',function(){
+        let val = $('#tahun').val();
+        if(val != "xx"){
+            $('#ftco-loader').addClass('show');
+            let download_tahun_url = "{{route('all_user_downlaod_donasi_tahun',['_tahunnn_'])}}";
+            download_tahun_url = download_tahun_url.replace('_tahunnn_',val);
+            $('#btn_unduh_tahun').attr('href',download_tahun_url);
+            $('#bulan').html('');
+            $('#bulan').append(`<option value="xx">Pilih bulan...</option>`);
+            let url = "{{route('all_user_get_bulan_donasi_per_tahun',['_year_'])}}"
+            url = url.replace('_year_', val);
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                for(let i = 0; i < data.length ; i++){
+                    $('#bulan').append(`
+                    <option value="${data[i].month}">${bulan[data[i].month]}</option>
+                    `);
+                }
+                $('#btn_unduh_bulan').addClass('disabled');
+                $('#btn_unduh_tahun').removeClass('disabled');
+                $('#form_bulan').removeAttr('disabled');
+                $('#ftco-loader').removeClass('show');
+            });
+        }else{
+            $('#btn_unduh_bulan').addClass('disabled');
+            $('#btn_unduh_tahun').addClass('disabled');
+            $('#btn_unduh_tahun').attr('href','#');
+            $('#form_bulan').attr('disabled','');
+        }
+    });
+
+    $('#bulan').on('change',function(){
+        let year = $('#tahun').val();
+        let val = $('#bulan').val();
+        if(val != "xx"){
+            let url_bln = "{{route('all_user_downlaod_donasi_bulan',['month'=>'__bulan','year'=>'_tahunnn_'])}}";
+            url_bln = url_bln.replace('__bulan',val);
+            url_bln = url_bln.replace('_tahunnn_',year);
+            $('#btn_unduh_bulan').attr('href',url_bln);
+            $('#btn_unduh_bulan').removeClass('disabled');
+        }else{
+            $('#btn_unduh_bulan').attr('href',"#");
+            $('#btn_unduh_bulan').addClass('disabled');
+        }
+    });
+});
+</script>
--- resources/views/frontend/pages/Home/home.blade.php
+++ resources/views/frontend/pages/Home/home.blade.php
@@ -4,93 +4,108 @@
 
 @section('CssTambahanAfter')
 <link rel="stylesheet" href="{{asset('admin/plugins/ion-rangeslider/css/ion.rangeSlider.min.css')}}">
-  <!-- bootstrap slider -->
+<!-- bootstrap slider -->
 <link rel="stylesheet" href="{{asset('admin/plugins/bootstrap-slider/css/bootstrap-slider.min.css')}}">
 
 <style>
-::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-  color: #666666 !important;
-  font-size: 0.8rem;
-  opacity: 0.1; /* Firefox */
-}
+    ::placeholder {
+        /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: #666666 !important;
+        font-size: 0.8rem;
+        opacity: 0.1;
+        /* Firefox */
+    }
 
-:-ms-input-placeholder { /* Internet Explorer 10-11 */
-  color: #343a40 !important;
-  font-size: 0.8rem;
-  opacity: 0.7;
-}
+    :-ms-input-placeholder {
+        /* Internet Explorer 10-11 */
+        color: #343a40 !important;
+        font-size: 0.8rem;
+        opacity: 0.7;
+    }
 
-::-ms-input-placeholder { /* Microsoft Edge */
-  color: #343a40 !important;
-  font-size: 0.8rem;
-  opacity: 0.7;
-}
+    ::-ms-input-placeholder {
+        /* Microsoft Edge */
+        color: #343a40 !important;
+        font-size: 0.8rem;
+        opacity: 0.7;
+    }
 
-.info-label {
-  font-size: 0.68rem;
-}
+    .info-label {
+        font-size: 0.68rem;
+    }
 
-.red {
-  color: red;
-}
+    .red {
+        color: red;
+    }
 
-@media only screen and (max-width: 992px)  {
-  .bg-resp{
-    background: none !important;
-  }
-}
+    @media only screen and (max-width: 992px) {
+        .bg-resp {
+            background: none !important;
+        }
+    }
 </style>
 @endsection
 
 
 @section('konten')
 <!--  -->
-<div class="hero-wrap" style="background-image: url('assets/aspiration/images/bg_4.jpg');" data-stellar-background-ratio="0.5">
-  <div class="overlay"></div>
-  <div class="container">
-    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center"
-      data-scrollax-parent="true">
-      <div class="col-md-12 text ftco-animate mt-5 text-center" data-scrollax=" properties: { translateY: '70%' }">
-        {{-- <div class="play-video d-flex align-items-center justify-content-center"><a href="https://vimeo.com/45830194"
+<div class="hero-wrap" style="background-image: url('assets/aspiration/images/bg_4.jpg');"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center"
+            data-scrollax-parent="true">
+            <div class="col-md-12 text ftco-animate mt-5 text-center"
+                data-scrollax=" properties: { translateY: '70%' }">
+                {{-- <div class="play-video d-flex align-items-center justify-content-center"><a href="https://vimeo.com/45830194"
             class="popup-vimeo"><span class="icon"><i class="ion-ios-play "></i></span></a></div> --}}
-        <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Ulurkan tanganmu untuk membuat dunia lebih baik</h1>
-      </div>
+                <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Ulurkan tanganmu untuk
+                    membuat dunia lebih baik</h1>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <section class="ftco-section ftco-no-pt ftco-no-pb ftco-volunteer">
-  <div class="container-fluid px-md-0">
-    <div class="row no-gutters">
-      <div class="bg-resp col-md-6 img-volunteer" style="background-image: url(assets/aspiration/images/baca_quran.jpg);">
-        <div class="row no-gutters justify-content-end">
-          <div class="col-lg-7">
-            <div class="text py-5 pl-md-4 pr-md-3">
-              <p style="font-size: 12px;">(Berinfaqlah) kepada orang-orang fakir yang terikat (oleh jihad) di jalan Allah; mereka tidak dapat (berusaha) di bumi; orang yang tidak tahu menyangka mereka orang kaya karena memelihara diri dari minta-minta. Kamu kenal mereka dengan melihat sifat-sifatnya, mereka tidak meminta kepada orang secara mendesak. Dan apa saja harta yang baik yang kamu nafkahkan (di jalan Allah), maka sesungguhnya Allah Maha Mengatahui.</p>
-              <h5>Quran Surat Al-Baqarah Ayat 273</h5>
+    <div class="container-fluid px-md-0">
+        <div class="row no-gutters">
+            <div class="bg-resp col-md-6 img-volunteer"
+                style="background-image: url(assets/aspiration/images/baca_quran.jpg);">
+                <div class="row no-gutters justify-content-end">
+                    <div class="col-lg-7">
+                        <div class="text py-5 pl-md-4 pr-md-3">
+                            <p style="font-size: 12px;">(Berinfaqlah) kepada orang-orang fakir yang terikat (oleh jihad)
+                                di jalan Allah; mereka tidak dapat (berusaha) di bumi; orang yang tidak tahu menyangka
+                                mereka orang kaya karena memelihara diri dari minta-minta. Kamu kenal mereka dengan
+                                melihat sifat-sifatnya, mereka tidak meminta kepada orang secara mendesak. Dan apa saja
+                                harta yang baik yang kamu nafkahkan (di jalan Allah), maka sesungguhnya Allah Maha
+                                Mengatahui.</p>
+                            <h5>Quran Surat Al-Baqarah Ayat 273</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="col-md-6 d-flex align-items-center bg-primary">
+                <div class="about-text px-3 py-5 pl-md-5 pr-md-5">
+                    <h2>Donasi Terkumpul <br><span>Rp</span>
+                        @if ($donasi)
+                        <strong class="number" data-number="{{$donasi->sum}}">0</strong>
+                        @else
+                        <strong class="number">0</strong>
+                        @endif
+                    </h2>
+                    <p>Donasi yang terkumpulkan merupakan donasi bulan ini yang di butuhkan anak-anak panti setiap bulan
+                        mulai dari sekolah, pakaian, uang jajan dan kebutuhan lainnya.
+                    </p>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-black py-3 px-4" data-toggle="modal"
+                        data-target="#donasiModal">
+                        Donasi Sekarang
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-md-6 d-flex align-items-center bg-primary">
-        <div class="about-text px-3 py-5 pl-md-5 pr-md-5">
-          <h2>Donasi Terkumpul <br><span>Rp</span>
-            @if ($donasi)
-              <strong class="number" data-number="{{$donasi->sum}}">0</strong>
-            @else
-              <strong class="number">0</strong>
-            @endif
-          </h2>
-          <p>Donasi yang terkumpulkan merupakan donasi bulan ini yang di butuhkan anak-anak panti setiap bulan mulai dari sekolah, pakaian, uang jajan dan kebutuhan lainnya.
-            </p>
-          <!-- Button trigger modal -->
-          <button type="button" class="btn btn-black py-3 px-4" data-toggle="modal" data-target="#donasiModal">
-            Donasi Sekarang
-          </button>
-        </div>
-      </div>
     </div>
-  </div>
 </section>
 
 <!-- Modal Donasi-->
@@ -101,26 +116,26 @@
 @include('frontend.pages.Home.Components.donasi_now')
 
 @if(count($last_3month) > 0)
-  <section class="ftco-section ftco-causes">
+<section class="ftco-section ftco-causes">
     <div class="container">
         {{-- {{$last_3month}} --}}
-      <div class="row">
-        @foreach ($last_3month as $item)
-          <div class="col-md-{{$jml_last_3month}}">
-            <div class="causes causes-2 text-center ftco-animate">
-            <h2>Donasi Bulan {{$item->month}}</h2>
-              <div class="goal">
-                <p><span>Rp <span class="number" data-number="{{$item->sum}}">0</span></span> terkumpul</p>
-                <div class="progress" style="height:20px">
-                  <div class="progress-bar progress-bar-striped" style="width:100%; height:20px">Selesai</div>
+        <div class="row">
+            @foreach ($last_3month as $item)
+            <div class="col-md-{{$jml_last_3month}}">
+                <div class="causes causes-2 text-center ftco-animate">
+                    <h2>Donasi Bulan {{$item->month}}</h2>
+                    <div class="goal">
+                        <p><span>Rp <span class="number" data-number="{{$item->sum}}">0</span></span> terkumpul</p>
+                        <div class="progress" style="height:20px">
+                            <div class="progress-bar progress-bar-striped" style="width:100%; height:20px">Selesai</div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        @endforeach
-      </div>
+            @endforeach
+        </div>
     </div>
-  </section>
+</section>
 @endif
 
 @include('frontend.pages.Home.Components.recent_blog')
@@ -129,7 +144,7 @@
 
 @section('JsTambahanAfter')
 <script src="{{asset('admin/plugins/ion-rangeslider/js/ion.rangeSlider.min.js')}}"></script>
-  <!-- Bootstrap slider -->
+<!-- Bootstrap slider -->
 <script src="{{asset('admin/plugins/bootstrap-slider/bootstrap-slider.min.js')}}"></script>
 
 <script src="{{asset('admin/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
